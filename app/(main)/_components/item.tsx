5a9db331e54b0cdb65cf9a86006111d6c93259e2
Sarthak Khandelwal <sarthakhandelwal@gmail.com>
2024-01-31 23:46:58 +0530

    sidebar item changes

--- app/(main)/_components/item.tsx
+++ app/(main)/_components/item.tsx
@@ -67,8 +67,8 @@ export const Item = ({
                 {label}
             </span>
             {isSearch && (
-                <kbd className="ml-auto pointer-events-none inline-flex h-5 select-none items-center gap-1 rounded border bg-muted px-1.5 font-mono text-[10px] font-medium text-muted-foreground">
-                    <span className="text-xs">⌘</span>+ K
+                <kbd className="ml-auto pointer-events-none inline-flex h-5 select-none items-center gap-1 rounded border dark:border-zinc-700 bg-muted px-1.5 font-mono text-[10px] font-medium text-muted-foreground">
+                    CTRL K
                 </kbd>
             )}
         </div>
